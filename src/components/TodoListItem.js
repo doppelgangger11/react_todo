001a6c11334a2ec511c0cf5b2d6b36b908c2b303
doppelgangger11 <gamenightmer@gmail.com>
2024-04-05 08:16:27 +0500

    step 7 props

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const TodoListItem = () => {
-  return <span>test</span>;
+const TodoListItem = ({ label, important = false }) => {
+
+  const style = {
+      color: important ? 'tomato' : 'black'
+  };
+  return <span style={ style }>{ label }</span>
+  //props argument in TodoListItem
+  //return <span>{ props.label }</span>
 };
 
 export default TodoListItem;
