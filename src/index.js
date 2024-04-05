86631af3efbcfb179a82015af8227e32dc16dded
doppelgangger11 <gamenightmer@gmail.com>
2024-04-05 07:37:17 +0500

    step 4

--- src/index.js
+++ src/index.js
@@ -4,16 +4,34 @@ import ReactDOM from "react-dom";
 // console.log("testing react app");
 
 //const element = React.createElement('hi', null, 'Hello React World');
-const element = (
-  <div>
-      <h1>My Todo List</h1>
-      <input placeholder="search" />
-      <ul>
-          <li>Install React</li>
-          <li>Study React</li>
-          <li>Use React</li>
-          <li>Build React App</li>
-      </ul>
-  </div>
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Install React</li>
+      <li>Study React</li>
+      <li>Use React</li>
+      <li>Build React App</li>
+      <li>But Python Is Better ¯\_(ツ)_/¯</li>
+    </ul>
   );
-  ReactDOM.render(element, document.getElementById('root'));
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
