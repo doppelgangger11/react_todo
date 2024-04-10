a25c129204c8eaa88e283cfe4192ed36d4877c73
doppelgangger11 <gamenightmer@gmail.com>
2024-04-10 14:16:05 +0500

    step 22 setState() modifications

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./TodoListItem.css";
+
 export default class TodoListItem extends Component {
   state = {
     done: false,
+    important: false,
   };
 
   onLabelClick = () => {
@@ -10,35 +12,37 @@ export default class TodoListItem extends Component {
       done: true,
     });
   };
+  onMarkImportant = () => {
+    this.setState({
+      important: true,
+    });
+  };
 
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = "todo-list-item";
     if (done) {
       classNames += " done";
     }
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
+    if (important) {
+      classNames += " important";
+    }
     return (
       <span className={classNames}>
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
+
         <button
           type="button"
           className="btn btn-outline-danger btn-sm float-right"
